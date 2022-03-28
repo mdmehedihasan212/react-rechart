d0766cfdecac9b3bbd77c346dc6a27ae6a25aaba
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-28 23:59:55 +0600

    add simple chart

--- src/components/MyLineChart/MyLineChart.js
+++ src/components/MyLineChart/MyLineChart.js
@@ -1,53 +1,61 @@
 import React from 'react';
+import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const MyLineChart = () => {
     const data = [
         {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
+            name: 'supplier A',
+            price: 4000,
+            sales: 2400,
             amt: 2400,
         },
         {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
+            name: 'supplier B',
+            price: 3000,
+            sales: 1398,
             amt: 2210,
         },
         {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
+            name: 'supplier C',
+            price: 2000,
+            sales: 9800,
             amt: 2290,
         },
         {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
+            name: 'supplier D',
+            price: 2780,
+            sales: 3908,
             amt: 2000,
         },
         {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
+            name: 'supplier E',
+            price: 1890,
+            sales: 4800,
             amt: 2181,
         },
         {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
+            name: 'supplier F',
+            price: 2390,
+            sales: 3800,
             amt: 2500,
         },
         {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
+            name: 'supplier G',
+            price: 3490,
+            sales: 4300,
             amt: 2100,
         },
     ];
     return (
         <div>
-
+            <LineChart width={800} height={400} data={data}>
+                <Line dataKey={'price'}></Line>
+                <Line dataKey={'sales'}></Line>
+                <XAxis dataKey={'name'}></XAxis>
+                <Tooltip></Tooltip>
+                <Legend></Legend>
+                <YAxis></YAxis>
+            </LineChart>
         </div>
     );
 };
